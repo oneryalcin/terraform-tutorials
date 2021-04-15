a832c7e14229edcfa6acf72467604a7f5e4bab49
Mehmet Oner Yalcin <oneryalcin@gmail.com>
2021-04-15 14:38:44 +0000

    Add variable validation

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -9,12 +9,37 @@ terraform {
 
 provider "docker" {}
 
+variable "ext_port" {
+  type    = number
+  default = 1880
+  
+  validation {
+    condition = var.ext_port <= 65535 && var.ext_port > 0
+    error_message = "The external port must be in the valid port range."
+  }
+}
+
+variable "int_port" {
+  type    = number
+  default = 1880
+    
+  validation {
+    condition = var.int_port == 1880
+    error_message = "The internal port must be 1880."
+  }
+}
+
+variable "container_cnt" {
+  type    = number
+  default = 1
+}
+
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
 resource "random_string" "random" {
-  count   = 2
+  count   = var.container_cnt
   length  = 4
   special = false
   upper   = false
@@ -22,24 +47,24 @@ resource "random_string" "random" {
 
 
 resource "docker_container" "nodered_container" {
-  count = 2
+  count = var.container_cnt
   name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
-    internal = 1880
-    # external = 1880
+    internal = var.int_port
+    external = var.ext_port
 
   }
 }
 
 
 output "ip-address" {
-    # [for i in docker_container.nodered_container[*]: join(":", [i.ip_address,i.ports[0].external]) ]
-    value = [for i in docker_container.nodered_container[*]: join(":", [i.ip_address],i.ports[*]["external"]) ]
-    description = "The IP address of the container"
+  # [for i in docker_container.nodered_container[*]: join(":", [i.ip_address,i.ports[0].external]) ]
+  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+  description = "The IP address of the container"
 }
 
 output "container-name" {
-    value = docker_container.nodered_container[*].name
-    description = "The name of the container"
+  value       = docker_container.nodered_container[*].name
+  description = "The name of the container"
 }
