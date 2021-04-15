911eedd10f7d200e4dfe7156199a54f89a918054
Mehmet Oner Yalcin <oneryalcin@gmail.com>
2021-04-15 15:30:25 +0000

    38 adding perm vlume for docker

--- terraform-docker/outputs.tf
+++ terraform-docker/outputs.tf
@@ -3,7 +3,7 @@ output "ip-address" {
   # [for i in docker_container.nodered_container[*]: join(":", [i.ip_address,i.ports[0].external]) ]
   value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
   description = "The IP address of the container"
-  sensitive = true
+  # sensitive = true
 }
 
 output "container-name" {
