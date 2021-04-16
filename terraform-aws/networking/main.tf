275cfd739840ff24ab509313a5a81180fc91a699
Mehmet Oner Yalcin <oneryalcin@gmail.com>
2021-04-17 00:07:13 +0100

    74 VPC Subnets Hardcoded

--- terraform-aws/networking/main.tf
+++ terraform-aws/networking/main.tf
@@ -15,3 +15,20 @@ resource "aws_vpc" "mtc_vpc" {
     Name = "mtc_vpc-${random_integer.random.id}"
   }
 }
+
+
+resource "aws_subnet" "mtc_public_subnet" {
+  count = length(var.public_cidrs)
+  cidr_block = var.public_cidrs[count.index]
+  vpc_id = aws_vpc.mtc_vpc.id
+  map_public_ip_on_launch = true
+  availability_zone = [
+    "eu-west-2a",
+    "eu-west-2b",
+    "eu-west-2c",
+  ][count.index]
+
+  tags = {
+    Name = "mtc-public_${count.index + 1}"
+  }
+}
